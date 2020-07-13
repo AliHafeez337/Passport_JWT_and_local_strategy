b17392cbae34579b6583f301983769aaba04d1f4
Ali Hafeez <52742115+AliHafeez337@users.noreply.github.com>
2020-07-13 14:12:19 +0500

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -1,18 +1,24 @@
 module.exports = {
   ensureAuthenticated: (req, res, next) => {
     if (req.isAuthenticated()) {
-      req.token = req.headers.authorization.slice(7)
-
-      for (let i in req.user.tokens){
-        if ( req.token === req.user.tokens[i].token ){
-          return next();
+      if (req.user.archived){
+        res.status(401).send({
+          'errmsg': "Unauthorized, You are archived."
+        });
+      } else {
+        req.token = req.headers.authorization.slice(7)
+  
+        for (let i in req.user.tokens){
+          if ( req.token === req.user.tokens[i].token ){
+            return next();
+          }
         }
       }
+    } else {
+      res.status(401).send({
+        'errmsg': "Unauthorized, You can not access this resource."
+      });
     }
-    req.flash('error_msg', 'Please log in to view that resource');
-    res.status(401).send({
-      'errmsg': "Unauthorized, You can not access this resource."
-    });
   },
   adminAuthenticated: (req, res, next) => {
     if (req.user.usertype === 'admin') {
